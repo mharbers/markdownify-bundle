51a1ff1bd77f535c31f800002c8c09272cda690e
Martijn Harbers <m.harbers@obbink.nl>
2016-10-12 17:27:21 +0200

    Initial commit

---
+++ src/MarkdownifyBundle.php
@@ -0,0 +1,8 @@
+<?php
+namespace Mharbers\MarkdownifyBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+final class MarkdownifyBundle extends Bundle
+{
+}
---
+++ src/README.md
@@ -0,0 +1,3 @@
+# markdownify-bundle
+
+Symfony 2 bundle to integrate pixel418/markdownify-library into your Symfony2 project.
